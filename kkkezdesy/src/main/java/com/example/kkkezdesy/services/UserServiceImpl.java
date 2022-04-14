00985a5e2bef24c4d3320dbf887bc2a1d252340c
sssilencce <79984310+sssilencce@users.noreply.github.com>
2022-04-14 22:50:49 +0600

    Add files via upload

---
+++ kkkezdesy/src/main/java/com/example/kkkezdesy/services/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.kkkezdesy.services;
+
+import com.example.kkkezdesy.entities.User;
+import com.example.kkkezdesy.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found in the database.");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
