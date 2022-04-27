f9e7704617e039b216547d2033fa71554ec401d4
Ayabek <Aitkazinn03@gmail.com>
2022-04-27 13:26:23 +0600

    2week

---
+++ kkkezdesy/src/main/java/com/example/kkkezdesy/controller/ProfileController.java
@@ -0,0 +1,57 @@
+package com.example.kkkezdesy.controller;
+
+import com.example.kkkezdesy.entities.User;
+import com.example.kkkezdesy.model.DeleteProfileRequest;
+import com.example.kkkezdesy.repositories.UserRepo;
+import jdk.jfr.DataAmount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProfileController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private MainController mainController;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @GetMapping("/getUser")
+    public ResponseEntity<User>getUserByUsername(@RequestParam String email) {
+        return ResponseEntity.ok().body(userRepo.findByEmail(email));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity updateProfile(@RequestBody User user){
+        int x = mainController.userIsValid(user);
+        User user2 = userRepo.findByEmail(user.getEmail());
+        if(x == 0 || x == 1){
+            user2.setFirst_name(user.getFirst_name());
+            user2.setLast_name(user.getLast_name());
+            user2.setAge(user.getAge());
+            user2.setGender(user.getGender());
+            user2.setCity(user.getCity());
+
+            userRepo.save(user2);
+
+        }else{
+            return ResponseEntity.badRequest().body("Some field is incorrect.");
+        }
+        return ResponseEntity.ok().body("User updated");
+    }
+
+    @PostMapping("/deleteUser")
+    public ResponseEntity deleteProfile(@RequestBody DeleteProfileRequest deleteProfileRequest){
+        User user = userRepo.findByEmail(deleteProfileRequest.getEmail());
+        userRepo.deleteById(user.getId());
+        return ResponseEntity.ok().body("User was deleted.");
+    }
+}
+
+
---
+++ kkkezdesy/src/main/java/com/example/kkkezdesy/controller/WebController.java
@@ -0,0 +1,34 @@
+package com.example.kkkezdesy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/auth")
+    public String getPage() {
+        return "register";
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/loginUser")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/profile")
+    public String getProfilePage() {
+        return "profile";
+    }
+
+    @GetMapping("/updateUser")
+    public String getEditPage() {
+        return "updateUser";
+    }
+}
